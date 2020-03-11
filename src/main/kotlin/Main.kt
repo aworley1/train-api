dbb622b3fdedf2ae5c4951c3eaaff37bc4126aaa
Andrew Worley <aworley1@googlemail.com>
2020-03-11 18:37:52 +0000

    Start server in main function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
-fun main() {
+import com.thalesgroup.rtti._2013_11_28.token.types.AccessToken
+import com.thalesgroup.rtti._2017_10_01.ldb.Ldb
+import org.http4k.server.Jetty
+import org.http4k.server.asServer
 
+fun main() {
+    createTrainRoute(
+            createGetTrains(
+                    ldbService = Ldb().ldbServiceSoap,
+                    accessToken = AccessToken().apply { tokenValue = "" }
+            )
+    ).asServer(Jetty(8080)).start()
 }
