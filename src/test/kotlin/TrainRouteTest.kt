aefd5b2331fe87ed043812c69e25ea8f33a6f75c
Andrew Worley <aworley1@googlemail.com>
2020-03-12 22:41:20 +0000

    Add test for train Json response

--- src/test/kotlin/TrainRouteTest.kt
+++ src/test/kotlin/TrainRouteTest.kt
@@ -1,5 +1,6 @@
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import assertk.assertions.isFalse
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
@@ -9,12 +10,21 @@ import org.http4k.core.Uri
 import org.http4k.core.query
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
+import org.http4k.format.Jackson.asJsonObject
 
 object TrainRouteTest : Spek({
 
     describe("train route") {
         val mockGetTrains = mockk<GetTrains>()
-        every { mockGetTrains(any(), any()) } returns emptyList()
+        every { mockGetTrains(any(), any()) } returns listOf(
+                Train(
+                        scheduledTimeOfDeparture = "scheduledTimeOfDeparture",
+                        estimatedTimeOfDeparture = "estimatedTimeOfDeparture",
+                        isCancelled = false,
+                        isCancelledAtDestination = false
+                )
+        )
+
         val route = createTrainRoute(mockGetTrains)
 
         val response = route(
@@ -28,8 +38,13 @@ object TrainRouteTest : Spek({
             verify { mockGetTrains("departureStation", "destinationStation") }
         }
 
-        it("should return an empty array") {
-            assertThat(response.bodyString()).isEqualTo("[]")
+        it("should return an array of trains") {
+            val firstTrain = response.bodyString().asJsonObject()[0]
+            assertThat(firstTrain["scheduledTimeOfDeparture"].asText()).isEqualTo("scheduledTimeOfDeparture")
+            assertThat(firstTrain["estimatedTimeOfDeparture"].asText()).isEqualTo("estimatedTimeOfDeparture")
+            assertThat(firstTrain["isCancelled"].asBoolean()).isFalse()
+            assertThat(firstTrain["isCancelledAtDestination"].asBoolean()).isFalse()
+            assertThat(firstTrain["status"].asText()).isEqualTo("DELAYED_OVER_THRESHOLD")
         }
     }
 })
