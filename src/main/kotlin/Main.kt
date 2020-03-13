efbbcc1818cc7fcdb3b44333702c892e88491e93
Andrew Worley <aworley1@googlemail.com>
2020-03-13 23:39:36 +0000

    Add CORS headers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,19 @@
 import com.thalesgroup.rtti._2013_11_28.token.types.AccessToken
 import com.thalesgroup.rtti._2017_10_01.ldb.Ldb
+import org.http4k.core.then
+import org.http4k.filter.CorsPolicy
+import org.http4k.filter.ServerFilters
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
 
 fun main() {
     val accessToken = System.getenv("NATIONAL_RAIL_TOKEN")
-    createTrainRoute(
-            createGetTrains(
-                    ldbService = Ldb().ldbServiceSoap,
-                    accessToken = AccessToken().apply { tokenValue = accessToken }
+    ServerFilters.Cors(CorsPolicy.UnsafeGlobalPermissive).then(
+            createTrainRoute(
+                    createGetTrains(
+                            ldbService = Ldb().ldbServiceSoap,
+                            accessToken = AccessToken().apply { tokenValue = accessToken }
+                    )
             )
     ).asServer(Jetty(8080)).start()
 }
